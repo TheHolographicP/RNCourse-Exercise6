8ff1ef87ac634d427f741018a5ac494cacbe2621
TheHolographicP <thegamblerkid@gmail.com>
2026-03-25 10:58:54 -0600

    Create place class;

---
+++ model/place.ts
@@ -0,0 +1,21 @@
+
+
+type Location = {
+    lat: number;
+    lng: number;
+}
+
+export class Place {
+    constructor(title: string, imageUri: string, address: string, location: Location) {
+        this.id = new Date().toString() + Math.random().toString();
+        this.title = title;
+        this.imageUri = imageUri;
+        this.address = address;
+        this.location = location;
+    }
+    id: string;
+    title: string;
+    imageUri: string;
+    address: string;
+    location: Location;
+}
