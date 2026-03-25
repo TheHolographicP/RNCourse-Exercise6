0f5663ec16be8ec14d5740d7e96cb587274f86fc
TheHolographicP <thegamblerkid@gmail.com>
2026-03-25 11:08:19 -0600

    Add skeleton screens;

---
+++ screens/AddPlace.tsx
@@ -0,0 +1,7 @@
+import { PlaceForm } from 'components/Places/PlaceForm';
+
+export function AddPlace() {
+    return (
+        <PlaceForm />
+    );
+}
---
+++ screens/AllPlaces.tsx
@@ -0,0 +1,9 @@
+import { PlacesList } from 'components/Places/PlacesList';
+import { View, Text } from 'react-native';
+
+
+export function AllPlaces() {
+  return (
+    <PlacesList places={[]} />
+  );
+}
