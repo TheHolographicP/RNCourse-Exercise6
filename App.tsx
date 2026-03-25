08a3b202e03ff74e813b76ee7be79ed238d6e80c
TheHolographicP <thegamblerkid@gmail.com>
2026-03-25 11:54:30 -0600

    Implement nav button on AllPlaces screen for adding place;

--- App.tsx
+++ App.tsx
@@ -6,15 +6,31 @@ import { AllPlaces } from 'screens/AllPlaces';
 import { AddPlace } from 'screens/AddPlace';
 import { PlaceDetails } from 'screens/PlaceDetails';
 import { Map } from 'screens/Map';
+
+import { IconButton } from 'components/IconButton';
+
 import { RootStackParamList } from 'types/navigation';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="AllPlaces" component={AllPlaces} options={{ title: 'Your Favorite Places' }} />
+        <Stack.Screen name="AllPlaces" component={AllPlaces} options={({navigation}) => ({
+          title: 'Your Favorite Places', 
+          headerRight: ({tintColor}) => (
+            <IconButton 
+              icon="add"
+              size={24}
+              onPress={() => navigation.navigate('AddPlace')}
+              iconColor={tintColor}
+            />
+          )
+
+          })} />
         <Stack.Screen name="AddPlace" component={AddPlace} options={{ title: 'Add a New Place' }} />
         <Stack.Screen name="PlaceDetails" component={PlaceDetails} options={{ title: 'Loading Place...' }} />
         <Stack.Screen name="Map" component={Map} options={{ title: 'Map' }} />
